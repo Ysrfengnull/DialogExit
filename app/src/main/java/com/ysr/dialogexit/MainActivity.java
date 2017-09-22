fa39a5bd2b336d40a1b09b11125c6173a82c231f
ysrfeng <1282855229@qq.com>
2017-09-22 08:47:42 +0800

    图片动画

--- app/src/main/java/com/ysr/dialogexit/MainActivity.java
+++ app/src/main/java/com/ysr/dialogexit/MainActivity.java
@@ -9,6 +9,9 @@ import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -16,8 +19,9 @@ public class MainActivity extends Activity {
     protected static final int UIEND = 101;
     private String TextDemo = "恭喜您！收到一封感谢信。。。";
     private TextView text;
+    private ImageView iv_msg;
     private char[] charArrays;
-//    private String len = "";
+    //    private String len = "";
     ForegroundColorSpan redSpan;
     SpannableStringBuilder builder;
 
@@ -26,26 +30,47 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.cTextView);
+        iv_msg = (ImageView) findViewById(R.id.iv_msg);
         text.setText(TextDemo);
         builder = new SpannableStringBuilder(text.getText().toString());
         redSpan = new ForegroundColorSpan(Color.rgb(255, 255, 255));
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
 
-        new Thread() {
-            public void run() {
-                try {
-                    charArrays = TextDemo.toCharArray();
-                    for (int i = 0; i <=charArrays.length; i++) {
-                        builder.setSpan(redSpan, 0, i, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//                        len = charArrays[i] + "";
-                        handler.sendEmptyMessage(UI);
-                        sleep(200);
-                    }
-                    handler.sendEmptyMessage(UIEND);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
-        }.start();
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Log.e("onAnimationEnd", "onAnimationEnd");
+
+                new Thread() {
+                    public void run() {
+                        try {
+                            charArrays = TextDemo.toCharArray();
+                            for (int i = 0; i <= charArrays.length; i++) {
+                                builder.setSpan(redSpan, 0, i, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+//                        len = charArrays[i] + "";
+                                handler.sendEmptyMessage(UI);
+                                sleep(200);
+                            }
+                            handler.sendEmptyMessage(UIEND);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        findViewById(R.id.iv_msg).startAnimation(shake);
+
+
     }
 
     Handler handler = new Handler() {
