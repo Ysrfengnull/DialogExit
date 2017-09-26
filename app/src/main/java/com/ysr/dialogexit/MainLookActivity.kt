77a9f7c24b9a3dd96ae2eced4495bcdf8e98a728
ysrfeng <1282855229@qq.com>
2017-09-26 10:13:02 +0800

    kotlin

---
+++ app/src/main/java/com/ysr/dialogexit/MainLookActivity.kt
@@ -0,0 +1,12 @@
+package com.ysr.dialogexit
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainLookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_look)
+    }
+}
